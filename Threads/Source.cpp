a749fd38595ce77c44219dbdf6631547441ec449
BuildTools <unconfigured@null.spigotmc.org>
2021-12-04 00:12:01 +0500

    CW

---
+++ Threads/Source.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<thread>
+#include<Windows.h>
+using namespace std;
+
+bool finish = false;
+
+void Hello()
+{
+	while (!finish)
+	{
+		cout << "Hello";
+		Sleep(100);
+	}
+}
+void World()
+{
+	while (!finish)
+	{
+		cout << "World";
+		Sleep(100);
+	}
+}
+
+void main()
+{
+	std::thread hello_thread(Hello);
+	std::thread world_thread(World);
+
+	cin.get();
+	finish = true;
+
+	world_thread.join();
+	hello_thread.join();
+}
